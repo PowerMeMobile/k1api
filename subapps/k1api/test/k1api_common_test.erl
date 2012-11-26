c6a008e0b2d77f6aebc5c4d90bc34437b178eb03
Anton Sizov <a.sizov@dev1team.net>
2012-11-26 17:19:57 +0300

    Improve oneapi interface test

--- subapps/k1api/test/k1api_common_test.erl
+++ subapps/k1api/test/k1api_common_test.erl
@@ -1,6 +1,9 @@
 -module(k1api_common_test).
 -compile(export_all).
 
+%% @TODO Implement test for criteria, correlator, exceptions & also
+%% check all json keys for presents & and its' values
+
 -include_lib ("etest_http/include/etest_http.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
@@ -18,8 +21,19 @@
 	end, [Args])).
 
 -define(json_value(Key, Response),
-	apply(fun(__Key, __Res) ->
-		__JsonStruct = etest_http_json:decode(__Res#etest_http_res.body),
+	apply(fun
+			(__Key, __JsonStructBin) when is_binary(__JsonStructBin) ->
+ 		__JsonStruct = etest_http_json:decode(__JsonStructBin),
+	    case etest_http_json:fetch(__Key, __JsonStruct, undefined) of
+	        undefined -> .erlang:error({json_val_undefined,
+			                    [{json_struct, __JsonStruct},
+								 {module,   ?MODULE},
+          					     {line,     ?LINE}] });
+	        Value -> Value
+	    end;
+
+			(__Key, __Res) ->
+ 		__JsonStruct = etest_http_json:decode(__Res#etest_http_res.body),
 	    case etest_http_json:fetch(__Key, __JsonStruct, undefined) of
 	        undefined -> .erlang:error({json_val_undefined,
 			                    [{module,   ?MODULE},
@@ -46,24 +60,43 @@ ver() -> "2".
 creds() ->
 	Creds = ?join([postpaid_customer(), delimiter(), user(), ":", pass()]),
 	base64:encode_to_string(Creds).
-correlator() -> "correlator".
+correlator() -> base64:encode_to_string(crypto:rand_bytes(14)).
 addr_preffix() -> ?join([prot(), host(), ":", port(), "/", ver()]).
+
+%% notify urls
+notify_port() -> "44444".
+sms_notify_url() ->
+	?join(["http://127.0.0.1:", notify_port(), "/incoming_sms"]).
+receipts_notify_url() ->
+	?join(["http://127.0.0.1:", notify_port(), "/receipts"]).
+
+
 
 %% ===================================================================
 %% Tests
 %% ===================================================================
 
-outbound_sms_postpaid_test() ->
+setup() ->
+	oneapi_incoming_srv:start().
 
-	%% Send sms request
+outbound_sms_postpaid_test_() ->
+	{setup,
+		fun setup/0,
+		{timeout, 60,
+			[?_test(outbound_sms_postpaid())]}}.
+
+outbound_sms_postpaid() ->
+
+	%% Send sms request with delivery receipts
 
 	Url = ?join([addr_preffix(), "/smsmessaging/outbound/", sender_addr(), "/requests"]),
+	NotifyURL = receipts_notify_url(),
 	Body = ?url_encode([{"address", "tel%3A%2B13500000991"},
 						{"address", "tel%3A%2B13500000992"},
 						{"senderAddress", sender_addr()},
 						{"message", message()},
 						{"clientCorrelator", correlator()},
-						{"notifyURL", "http://application.example.com/notifications/DeliveryInfoNotification"},
+						{"notifyURL", NotifyURL},
 						{"callbackData", "some-data-useful-to-the-requester"},
 						{"senderName", "ACME%20Inc."}]),
 	Headers = [{"Authorization", creds()}],
@@ -79,7 +112,13 @@ outbound_sms_postpaid_test() ->
 	SmsStResponse = ?perform_get(SmsStUrl, Headers),
 	?assert_status(200, SmsStResponse),
 	?assert_json_key([<<"deliveryInfoList">>, <<"deliveryInfo">>], SmsStResponse),
-	?assert_json_value([<<"deliveryInfoList">>, <<"resourceURL">>], list_to_binary(SmsStUrl), SmsStResponse).
+	?assert_json_value([<<"deliveryInfoList">>, <<"resourceURL">>], list_to_binary(SmsStUrl), SmsStResponse),
+
+	%% Retrive receipts from test http server
+
+	timer:sleep(3000),
+	{ok, GotReceipts} = oneapi_incoming_srv:give_receipts(),
+	?assertEqual(2, length(GotReceipts)).
 
 %% add send sms & then retrieve sms again
 retrieve_sms_test() ->
@@ -92,22 +131,67 @@ retrieve_sms_test() ->
 	?assert_json_key([<<"inboundSMSMessageList">>, <<"resourceURL">>], Resp),
 	?assert_json_key([<<"inboundSMSMessageList">>, <<"totalNumberOfPendingMessages">>], Resp).
 
-incoming_sms_sub_test() ->
+incoming_sms_sub_test_() ->
+	{setup,
+		fun setup/0,
+		{timeout, 60,
+			[?_test(incoming_sms_sub())]}}.
+
+incoming_sms_sub() ->
 
 	%% Subscribe
 
+	CallbackData = "doSomething()",
 	Url = ?join([addr_preffix(), "/smsmessaging/inbound/subscriptions"]),
 	Body = ?url_encode([
 						{"destinationAddress", k1api_client_msisdn()},
-						{"notifyURL", "http://localhost:33333/notifications/DeliveryInfoNotification"},
-						{"criteria", "TAG"},
+						{"notifyURL", sms_notify_url()},
+						%% {"criteria", "TAG"},
 						{"notificationFormat", "JSON"},
-						{"callbackData", "doSomething()"},
+						{"callbackData", CallbackData},
 						{"clientCorrelator", correlator()}
 						]),
 	Headers = [{"Authorization", creds()}],
     Response = ?perform_post(Url, Headers, Body),
     ?assert_status(201, Response),
+
+
+	%% Send incoming sms via SmppSim
+
+	SendInSmsUrl = "http://localhost:8071/inject_mo",
+	TestIncomingSmsText = "TestOneApiIncomingSms",
+	Queries = [{"short_message", TestIncomingSmsText},
+				{"source_addr", "375441254746"},
+				{"destination_addr", "375296660003"},
+				{"source_addr_ton", "1"},
+				{"source_addr_npi", "0"},
+				{"dest_addr_ton", "1"},
+				{"dest_addr_npi", "1"}],
+	SendInSmsResponse = ?perform_get(SendInSmsUrl, [], Queries),
+	?assert_status(200, SendInSmsResponse),
+
+	%% Receive incoming sms
+
+	timer:sleep(3000), %% wait for delivery
+	{ok, IncomingSMSes} = oneapi_incoming_srv:give_sms(),
+	?assertEqual(1, length(IncomingSMSes)),
+	[IncomingSMS] = IncomingSMSes,
+	DebugCallbackData = ?json_value([<<"inboundSMSMessageNotification">>, <<"callbackData">>], IncomingSMS),
+	?assertEqual(DebugCallbackData, list_to_binary(CallbackData)),
+	DebugMessageText = ?json_value([<<"inboundSMSMessageNotification">>, <<"inboundSMSMessage">>, <<"message">>], IncomingSMS),
+	?assertEqual(DebugMessageText, list_to_binary(TestIncomingSmsText)),
+	?json_value([<<"inboundSMSMessageNotification">>,
+				<<"inboundSMSMessage">>,
+				<<"dateTime">>], IncomingSMS),
+	?json_value([<<"inboundSMSMessageNotification">>,
+				<<"inboundSMSMessage">>,
+				<<"destinationAddress">>], IncomingSMS),
+	?json_value([<<"inboundSMSMessageNotification">>,
+				<<"inboundSMSMessage">>,
+				<<"messageId">>], IncomingSMS),
+	?json_value([<<"inboundSMSMessageNotification">>,
+				<<"inboundSMSMessage">>,
+				<<"senderAddress">>], IncomingSMS),
 
 	%% Unsubscribe
 
@@ -122,7 +206,7 @@ sms_receipts_sub_test() ->
 	%% Subscribe
 
 	Url = ?join([addr_preffix(), "/smsmessaging/outbound/", k1api_client_msisdn(), "/subscriptions"]),
-	NotifyURL = "http://localhost:33333/notifications/DeliveryInfoNotification",
+	NotifyURL = receipts_notify_url(),
 	Criteria = "Tag",
 	CallbackData = "doSomething()",
 	Correlator = correlator(),
---
+++ subapps/k1api/test/oneapi_incoming_srv.erl
@@ -0,0 +1,136 @@
+-module(oneapi_incoming_srv).
+
+-behaviour(gen_server).
+-behaviour(cowboy_http_handler). %% here conflict of behaviours, but it isn't matter now
+
+%% API
+-export([
+	start/0,
+	give_sms/0,
+	give_receipts/0
+]).
+
+%% Cowboy Callbacks
+-export([
+	init/3,
+	handle/2
+	%% terminate/2
+]).
+
+%% GenServer Callbacks
+-export([
+	init/1,
+	handle_cast/2,
+	handle_call/3,
+	handle_info/2,
+	code_change/3,
+	terminate/2
+]).
+
+-record(st, {
+	sms = [] 		:: binary(),
+	receipts = [] 	:: binary()
+}).
+
+-define(PORT, 44444).
+
+%% ===================================================================
+%% API
+%% ===================================================================
+
+start() ->
+	gen_server:start({local, ?MODULE}, ?MODULE, [], []).
+
+give_sms() ->
+	gen_server:call(?MODULE, give_sms).
+
+give_receipts() ->
+	gen_server:call(?MODULE, give_receipts).
+
+save_sms(Sms) ->
+	gen_server:cast(?MODULE, {save_sms, Sms}).
+
+save_receipt(Receipt) ->
+	gen_server:cast(?MODULE, {save_receipt, Receipt}).
+
+%% ===================================================================
+%% Cowboy Callbacks
+%% ===================================================================
+
+init({tcp, http}, Req, [Type]) ->
+    {ok, Req, Type}.
+
+handle(Req, State = receipts) ->
+	{Method, _} = cowboy_http_req:method(Req),
+	case Method of
+		'POST' ->
+			{ok, Body, _} = cowboy_http_req:body(Req),
+			save_receipt(Body),
+		    {ok, Req2} = cowboy_http_req:reply(201, [], <<"Created">>, Req),
+		    {ok, Req2, State};
+		_Any ->
+		    {ok, Req2} = cowboy_http_req:reply(400, [], <<"Improper method">>, Req),
+		    {ok, Req2, State}
+	end;
+handle(Req, State = incoming_sms) ->
+	{Method, _} = cowboy_http_req:method(Req),
+	case Method of
+		'POST' ->
+			{ok, Body, _} = cowboy_http_req:body(Req),
+			save_sms(Body),
+		    {ok, Req2} = cowboy_http_req:reply(201, [], <<"Created">>, Req),
+		    {ok, Req2, State};
+		_Any ->
+		    {ok, Req2} = cowboy_http_req:reply(400, [], <<"Improper method">>, Req),
+		    {ok, Req2, State}
+	end.
+
+%% terminate(Req, State) ->
+%%     ok.
+
+%% ===================================================================
+%% GenServer Callbacks
+%% ===================================================================
+
+init([]) ->
+	ok = application:start(cowboy),
+	Dispatch = [
+	    %% {Host, list({Path, Handler, Opts})}
+	   	{'_', [
+			%% REST API
+            {[<<"incoming_sms">>], ?MODULE, [incoming_sms]},
+            {[<<"receipts">>], ?MODULE, [receipts]}
+			]}],
+	%% Name, NbAcceptors, Transport, TransOpts, Protocol, ProtoOpts
+	cowboy:start_listener(my_http_listener, 1,
+	    cowboy_tcp_transport, [{port, ?PORT}],
+	    cowboy_http_protocol, [{dispatch, Dispatch}]
+	),
+	{ok, #st{}}.
+
+handle_call(give_sms, _From, State = #st{sms = Sms}) ->
+	{reply, {ok, Sms}, State};
+handle_call(give_receipts, _From, State = #st{receipts = Receipts}) ->
+	{reply, {ok, Receipts}, State};
+handle_call(_Request, _From, State) ->
+    {stop, unexpected_call, State}.
+
+handle_cast({save_sms, Sms}, State = #st{sms = SMSes}) ->
+	{noreply, State#st{sms = [Sms | SMSes]}};
+handle_cast({save_receipt, Receipt}, State = #st{receipts = Receipts}) ->
+	{noreply, State#st{receipts = [Receipt | Receipts]}};
+handle_cast(_Msg, State) ->
+    {stop, unexpected_cast, State}.
+
+handle_info(_Info, State) ->
+    {stop, unexpected_info, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ===================================================================
+%% Internal
+%% ===================================================================
