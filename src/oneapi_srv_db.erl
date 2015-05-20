03e865bd2e5204058e717ad82963f4533dbdf25d
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-05-20 14:38:18 +0300

    Distinguish customer_id and customer_uuid. Cleanup

--- src/oneapi_srv_db.erl
+++ src/oneapi_srv_db.erl
@@ -24,19 +24,19 @@
 -include_lib("alley_common/include/logging.hrl").
 -include_lib("alley_common/include/gen_server_spec.hrl").
 
--type customer_id()   :: binary().
+-type customer_uuid() :: binary().
 -type user_id()       :: binary().
 -type correlator_id() :: binary().
 -type request_id()    :: binary().
 
-%% Customer Last MessageID Storage
+%% Customer Last MessageId Storage
 -record(customer_next_message_id, {
-    customer_id :: customer_id(),
+    customer_uuid :: customer_uuid(),
     next_id     :: integer()
 }).
 
 %% Customer Request Correlator Storage
--type correlator_key()   :: {customer_id(), user_id(), correlator_id()}.
+-type correlator_key()   :: {customer_uuid(), user_id(), correlator_id()}.
 -type correlator_value() :: request_id().
 
 -record(correlator, {
@@ -58,50 +58,50 @@
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
--spec next_id(customer_id()) -> {ok, [integer()]}.
-next_id(CustomerID) ->
-    next_id(CustomerID, 1).
+-spec next_id(customer_uuid()) -> {ok, [integer()]}.
+next_id(CustomerUuid) ->
+    next_id(CustomerUuid, 1).
 
--spec next_id(customer_id(), NumberOfIDs :: integer()) -> {ok, [integer()]}.
-next_id(CustomerID, NumberOfIDs) ->
-    {atomic, IDs} = mnesia:transaction(fun() ->
-        case mnesia:read(customer_next_message_id, CustomerID, write) of
+-spec next_id(customer_uuid(), NumberOfIds :: integer()) -> {ok, [integer()]}.
+next_id(CustomerUuid, NumberOfIds) ->
+    {atomic, Ids} = mnesia:transaction(fun() ->
+        case mnesia:read(customer_next_message_id, CustomerUuid, write) of
             [] ->
-                update_counter(1, NumberOfIDs, CustomerID);
-            [#customer_next_message_id{next_id = NextID}] ->
-                update_counter(NextID, NumberOfIDs, CustomerID)
+                update_counter(1, NumberOfIds, CustomerUuid);
+            [#customer_next_message_id{next_id = NextId}] ->
+                update_counter(NextId, NumberOfIds, CustomerUuid)
         end
     end),
-    {ok, IDs}.
+    {ok, Ids}.
 
 -spec write_correlator(
-    customer_id(), user_id(), undefined | correlator_id(), request_id()
+    customer_uuid(), user_id(), undefined | correlator_id(), request_id()
 ) -> ok | {error, {already_exists, request_id()}}.
 write_correlator(_, _, undefined, _) -> ok;
-write_correlator(CustomerID, UserID, CorrelatorID, NewRequestID) ->
+write_correlator(CustomerUuid, UserId, CorrelatorId, NewRequestId) ->
     {atomic, Result} = mnesia:transaction(fun() ->
-        Key = {CustomerID, UserID, CorrelatorID},
+        Key = {CustomerUuid, UserId, CorrelatorId},
         case mnesia:read(correlator, Key, write) of
             [] ->
                 CreatedAt = ac_datetime:utc_unixepoch(),
                 mnesia:write(#correlator{
                     key = Key,
-                    value = NewRequestID,
+                    value = NewRequestId,
                     created_at = CreatedAt
                 }),
                 ok;
-            [#correlator{value = RequestID}] ->
-                {error, {already_exists, RequestID}}
+            [#correlator{value = RequestId}] ->
+                {error, {already_exists, RequestId}}
         end
     end),
     Result.
 
 -spec delete_correlator(
-    customer_id(), user_id(), undefined | correlator_id()
+    customer_uuid(), user_id(), undefined | correlator_id()
 ) -> ok.
 delete_correlator(_, _, undefined) -> ok;
-delete_correlator(CustomerID, UserID, CorrelatorID) ->
-    Key = {CustomerID, UserID, CorrelatorID},
+delete_correlator(CustomerUuid, UserId, CorrelatorId) ->
+    Key = {CustomerUuid, UserId, CorrelatorId},
     ok = mnesia:dirty_delete(correlator, Key).
 
 %% ===================================================================
@@ -123,9 +123,9 @@ handle_info(timeout, State) ->
     MatchHead = #correlator{key = '$1', value = '_', created_at = '$2'},
     Guard = {'<', '$2', TimeThreshold},
     Result = '$1',
-    Delete = fun(ID) -> ok = mnesia:dirty_delete(correlator, ID) end,
-    IDs = mnesia:dirty_select(correlator, [{MatchHead, [Guard], [Result]}]),
-    lists:foreach(Delete, IDs),
+    Delete = fun(Id) -> ok = mnesia:dirty_delete(correlator, Id) end,
+    Ids = mnesia:dirty_select(correlator, [{MatchHead, [Guard], [Result]}]),
+    lists:foreach(Delete, Ids),
     {noreply, State, ?PurgeRate};
 
 handle_info(_Info, State) ->
@@ -141,19 +141,19 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal
 %% ===================================================================
 
-update_counter(NextID, NumberOfIDs, CustomerID) ->
+update_counter(NextId, NumberOfIds, CustomerUuid) ->
     Max = 999999999,
-    {From, To, NewNextID} =
-    case (NextID + NumberOfIDs - 1) > Max of
-        true -> {1, NumberOfIDs, NumberOfIDs + 1};
-        false -> {NextID, NextID + NumberOfIDs - 1, NextID + NumberOfIDs}
+    {From, To, NewNextId} =
+    case (NextId + NumberOfIds - 1) > Max of
+        true -> {1, NumberOfIds, NumberOfIds + 1};
+        false -> {NextId, NextId + NumberOfIds - 1, NextId + NumberOfIds}
     end,
-    IDs = lists:seq(From, To),
+    Ids = lists:seq(From, To),
     mnesia:write(#customer_next_message_id{
-        customer_id = CustomerID,
-        next_id = NewNextID
+        customer_uuid = CustomerUuid,
+        next_id = NewNextId
     }),
-    IDs.
+    Ids.
 
 init_mnesia() ->
     Nodes = [node()],
